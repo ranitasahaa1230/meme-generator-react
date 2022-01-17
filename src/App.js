6720eb4a3e29a09d415259ea8ae3ec08dbfe312f
Ranita Saha <saharanitaa1230@gmail.com>
2022-01-17 20:26:32 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1> Learn React</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
